22b996fa3ed82978cf45c22aa348f0c2eebaef08
jim <chemuku.jimmy@gmail.com>
2026-09-02 16:11:13 +0300

    feat: Modify to use async api

--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,9 @@
-from playwright.sync_api import Page, sync_playwright
+from playwright.async_api import Page, async_playwright
 
 from src.data import get_data
 
 
-def get_links(company: dict[str, str], page: Page) -> list[str]:
+async def get_links(company: dict[str, str], page: Page) -> list[str]:
     """Retrieve links from a company's job board
 
     Args:
@@ -15,14 +15,15 @@ def get_links(company: dict[str, str], page: Page) -> list[str]:
     """
     links: list[str] = []
     # Visit the company's job listing site
-    page.goto(company["url"])
+    await page.goto(company["url"])
     # Wait for page to finish loading
-    page.wait_for_load_state("networkidle")
+    await page.wait_for_load_state("networkidle")
     # Extract all career links
-    located = page.locator(company["descriptor"]).all()
+    located = page.locator(company["descriptor"])
+    count = await located.count()
 
-    for link in located:
-        href = link.get_attribute(company["attribute"])
+    for link in range(count):
+        href = await located.nth(link).get_attribute(company["attribute"])
 
         if href:
             links.append(href)
@@ -30,7 +31,7 @@ def get_links(company: dict[str, str], page: Page) -> list[str]:
     return links
 
 
-def get_listings(selector: str, links: list[str], page: Page) -> list[str]:
+async def get_listings(selector: str, links: list[str], page: Page) -> list[str]:
     """Retrieve the inner text of job postings.
 
     Args:
@@ -45,46 +46,46 @@ def get_listings(selector: str, links: list[str], page: Page) -> list[str]:
 
     for link in links:
         # Visit the posting
-        page.goto(link)
+        await page.goto(link)
         # Wait for idle network traffic
-        page.wait_for_load_state("networkidle")
+        await page.wait_for_load_state("networkidle")
 
         # Extract elements containing the same selector
         blocks = page.locator(selector)
         # Extract all the texts from all elements
-        texts = [blocks.nth(i).inner_text() for i in range(blocks.count())]
+        texts = [await blocks.nth(i).inner_text() for i in range(await blocks.count())]
         # Select the largest text containing the actual description
         listings.append(max(texts, key=len, default="").replace("\n", " "))
     return listings
 
 
-def get_jobs():
+async def get_jobs():
     """Retrieve job postings from all defined companies
 
     Returns:
         list[str]: A list of all companies with their available job postings
     """
     jobs: list[str] = []
-    with sync_playwright() as p:
+    async with async_playwright() as p:
         # Launch browser and open a page
-        browser = p.webkit.launch()
-        page = browser.new_page()
+        browser = await p.webkit.launch()
+        page = await browser.new_page()
         # Get a list of all companies
         companies = get_data("companies")
         for company in companies:
             print(f"Fetching from {company['name']}")
-            links = get_links(company, page)
+            links = await get_links(company, page)
 
             if len(links) == 0:
                 print(f"No listings from {company['name']}")
                 continue
-            listings = get_listings(company["content_selector"], links, page)
+            listings = await get_listings(company["content_selector"], links, page)
             for url, text in zip(links, listings):
                 jobs.append(f"""
                 Company Name: {company["name"]}
                 URL: {url}
                 Raw text: {text}
                 """)
-        browser.close()
+        await browser.close()
 
     return jobs
