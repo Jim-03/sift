bc688c7fda26faf814abd04fac81260e834b0742
jim <chemuku.jimmy@gmail.com>
2026-09-03 11:27:26 +0300

    feat: Add Job model and implement job extraction functionality in Gemini class

---
+++ src/dto.py
@@ -0,0 +1,57 @@
+from datetime import date, timedelta
+
+from pydantic import BaseModel, Field
+
+
+class Job(BaseModel):
+    is_valid_job: bool = Field(
+        description="Property to confirm if the provided data is valid job description"
+    )
+    company_name: str = Field(
+        default="Unknown", description="The name of the company offering the position"
+    )
+    source_url: str = Field(
+        description="The link from which the job posting was extracted"
+    )
+    location: str = Field(
+        default="Unknown",
+        description="The location where the posting/company is located",
+        examples=["Nairobi, Kenya", "Remote", "Kenya"],
+    )
+    title: str = Field(
+        description="The role name", examples=["Junior Software developer"]
+    )
+    salary: str = Field(
+        default="Unknown",
+        description="The salary range in the format (min - max) or the actual salary",
+        examples=["44,000 - 55,000", "50,000"],
+    )
+    currency: str = Field(
+        default="Unknown",
+        description="The currency of the salary",
+        examples=["KES", "USD"],
+    )
+    description: str = Field(description="The overall job description")
+    benefits: list[str] | None = Field(
+        default=None, description="A list of benefits of working in that job"
+    )
+    requirements: list[str] = Field(
+        description="A list of requirements an applicant needs to apply for the job"
+    )
+    date_posted: str = Field(
+        default_factory=lambda: date.today().strftime("%d-%m-%Y"),
+        description="The date this posting was made in dd-mm-YYYY format",
+    )
+    deadline: str = Field(
+        default_factory=lambda: (date.today() + timedelta(days=30)).strftime(
+            "%d-%m-%Y"
+        ),
+        description="The last date to apply in dd-mm-YYYY format",
+    )
+    notes: list[str] | None = Field(
+        description="Additional notes while applying for the job. For example the application process, expected documents etc."
+    )
+
+
+class JobListings(BaseModel):
+    jobs: list[Job] = Field(description="A list of jobs")
--- src/gemini.py
+++ src/gemini.py
@@ -1,8 +1,11 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 from google.genai import Client
 from google.genai.types import ContentEmbedding
+
+from dto import JobListings
 
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
@@ -35,3 +38,56 @@ class Gemini:
             raise Exception("Embedding failed!")
 
         return embeddings
+
+    def extract(self, jobs: list[str]) -> JobListings:
+        """Retrieve a list of jobs from scraped content
+
+        Args:
+            jobs (list[str]): A list of scrape content
+
+        Returns:
+            (JobListings): An object containing a list of job objects
+        """
+        if not jobs:
+            raise Exception("No jobs content provided!")
+
+        SYSTEM_PROMPT = f"""
+        You are a helpful extraction assistant. You have been provided a list of
+        scraped HTML content containing a job description. Your task is to extract
+        the job description from the content following the strict rules at 
+        </importantInstructions>
+        
+        <importantInstructions>
+        1. Extract only the provided job details from the scraped data.
+        2. For any content that doesn't contain a job description, set the 
+           'is_valid_job' property to false.
+        3. **DO NOT** generate any content that isn't provided in the description.
+        4. Dates may always be mixed up between month and  day, check the date at
+           </date> to determine the difference.
+        5. All date formats should be in dd-mm-YYYY format  
+        </importantInstructions>
+        
+        <date>
+        The current date in dd-mm-YYYY format is {datetime.now().today().strftime("%d-%m-%Y")}
+        </date>
+        """
+
+        interactions = self.__client.interactions.create(
+            model="gemini-3.1-flash-lite",
+            system_instruction=SYSTEM_PROMPT,
+            input=", ".join(jobs),
+            store=False,
+            generation_config={"temperature": 0.1},
+            response_format={
+                "type": "text",
+                "mime_type": "application/json",
+                "schema": JobListings.model_json_schema(),
+            },
+        )
+
+        results = interactions.output_text
+
+        if not results:
+            raise Exception("Gemini failed to generate response")
+
+        return JobListings.model_validate_json(results)
